deded27f55c17a2c8be201aeac5d680c32d719f8
olofk <olofk@DESKTOP-KQKFEJF>
2021-10-26 21:47:57 +0200

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.title = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.rating = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.title,
+            this.rating});
+            this.dataGridView1.Location = new System.Drawing.Point(13, 85);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(208, 336);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // title
+            // 
+            this.title.HeaderText = "title";
+            this.title.Name = "title";
+            // 
+            // rating
+            // 
+            this.rating.HeaderText = "rating";
+            this.rating.Name = "rating";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(408, 85);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(286, 336);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(706, 433);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn title;
+        private System.Windows.Forms.DataGridViewTextBoxColumn rating;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        List<Movie> PMovies = new List<Movie>(); // Global list so we can re-order it easily if wanted.
+
+        string Link = "https://www.imdb.com/chart/top/?ref_=nv_mv_250"; // Imdb top 250 rated url 
+        string Xpath = "/html/body/div[4]/div/div[2]/div[3]/div/div[1]/div/span/div/div/div[3]/table/tbody/tr[position()>0]"; // Xpath of the first node inside the parent list.
+
+
+        /*these all are different xpaths to different child nodes of our tbody/tr parent. Only copy what comes after our parent node, otherwise 
+
+         /html/body/div[4]/div/div[2]/div[3]/div/div[1]/div/span/div/div/div[3]/table/tbody/tr[1]/td[2]/a
+         /html/body/div[4]/div/div[2]/div[3]/div/div[1]/div/span/div/div/div[3]/table/tbody/tr[1]/td[3]/strong
+         /html/body/div[4]/div/div[2]/div[3]/div/div[1]/div/span/div/div/div[3]/table/tbody/tr[1]/td[1]/a/img
+
+         what we use will be using here is 
+
+        : td[2]/a
+        : td[3]/strong
+        : td[1]/a/img
+
+
+        */ 
+
+        
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadMovies(); // load when the application starts, if you do large amounts of scraping then the application will freeze. To fix this would be running for example LoadMovies() inside a thread. 
+        }
+
+        void LoadMovies()
+        {
+            var Movies = new List<Movie>(); // local list 
+
+            var Web = new HtmlWeb(); // New html webclient 
+
+            var Dock = Web.Load(Link); // Download and load the html of our url/link
+
+            var Nodes = Dock.DocumentNode.SelectNodes(Xpath); // Get all of the nodes that has the same xpath as our variable, with the exception of 
+
+            foreach(var Node in Nodes) // Extract information from each node <tr>
+            {
+                try
+                {
+                    var Movie = new Movie // Store locally 
+                    {
+                        Name = Node.SelectSingleNode("td[2]/a").InnerText, // title of movie 
+                        Rating = Node.SelectSingleNode("td[3]/strong").InnerText, // rating of movie 
+                        Img = Node.SelectSingleNode("td[1]/a/img").Attributes["src"].Value // image of movie (very low res) 47px
+                    };
+
+                    Movies.Add(Movie); // add to local list
+
+                } catch { } // Easy handle of exception
+            }
+
+            // Loop through local list to add into datagridview 
+
+            foreach (var Movie in Movies)
+            {
+                dataGridView1.Rows.Add(Movie.Name, Movie.Rating);
+            }
+            // add local list to global list,  I know it looks retarded.
+            PMovies.Clear();
+            PMovies.AddRange(Movies); 
+        }
+        
+
+        class Movie
+        {
+            public string Name { get; set; }
+
+            public string Rating { get; set; }
+
+            public string Img { get; set; }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                pictureBox1.ImageLocation = PMovies[dataGridView1.CurrentCell.RowIndex].Img; // set the picturebox image to whatever the user clicks on.
+            } catch { } // 
+        }
+    }
+}
